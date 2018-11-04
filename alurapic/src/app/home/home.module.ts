d3fca00b15ff5a430555fd86a8aa881fff19cb35
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-11-04 17:09:00 -0200

    Criação da tela de login, redirecionamento e componentização dos elementos da tela

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [SignInComponent]
+    declarations: [SignInComponent],
+    imports: [ReactiveFormsModule, CommonModule, VMessageModule, RouterModule]
 })
 export class HomeModule {
 
