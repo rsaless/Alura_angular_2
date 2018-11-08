a80f6981335e147a3ee28496f1e9a5e55d9434f8
Rafael Sales Pavarina <rafael.pavarina@catijr.com.br>
2018-11-08 03:40:26 -0200

    início das validações no formulário de cadastro

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [SignInComponent],
-    imports: [ReactiveFormsModule, CommonModule, VMessageModule, RouterModule]
+    declarations: [SignInComponent, SignupComponent],
+    imports: [ReactiveFormsModule, CommonModule, VMessageModule, RouterModule, FormsModule]
 })
 export class HomeModule {
 
